43db6f2810dd486a3b9baa0dc4f3077fc0a50e5b
ferianai <ferianaisti@gmail.com>
2025-07-27 19:02:43 +0700

    chore: update auth service token access as string

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,9 @@ def create_app(config_module="config.local"):
 
     # Register blueprints
     app.register_blueprint(index_router)
+
     app.register_blueprint(product_bp)
+
     app.register_blueprint(auth_bp, url_prefix="/auth")
 
     return app
